448397802013b207c8ec9ff9576cf723a7afacf2
SuHyeon Jeong <119032609+kmujsh@users.noreply.github.com>
2023-11-24 17:01:06 +0900

    수열과 구간 쿼리 4 / Level0 / 10분

---
+++ Level0/수열과 구간 쿼리 4.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer = arr;
+    for(int i = 0; i < queries.size(); i++) {
+        int s = queries[i][0];
+        int e = queries[i][1];
+        int k = queries[i][2];
+        for(int j = s; j <= e; j++)
+            if(j % k == 0) answer[j] += 1;
+    }
+    return answer;
+}
